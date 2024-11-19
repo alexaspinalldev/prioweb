35be4cc396a4d3d912c90dcb7a0059b2cca2e4fe
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2024-11-19 20:04:40 +0000

    Sketch js script

---
+++ script.js
@@ -0,0 +1,26 @@
+// Element references
+const taskInput = document.getElementById("taskInput")
+const addNewTaskBtn = document.getElementById("addNewTaskBtn")
+const openTasks = document.getElementById("openTasks")
+const completeTasks = document.getElementById("completeTasks")
+
+// Event listeners to catch the Add Task click
+    // button click
+    // key press
+
+// Function add task
+    // check if it's the enter key 13
+    // Check inpu text value is true
+    // Create new li item
+    // Append the list
+    // Clear text box
+    // Focus on text box
+
+
+// Event listener to catch the Complete Task click
+
+// Function complete task (move it to the comnplete list)
+
+// Event listener to catch the Delete Task click (If this is oging to be per list item it needs to be in that function)
+
+// Function delete task
