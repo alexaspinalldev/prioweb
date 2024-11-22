f07e6371515f5c3599b5bd2c6c55e7f7e943ab12
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2024-11-22 13:20:44 +0000

    Add listeners for new buttons within create function

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
-// New tasks
+// New tasks button listener
 const addNewTaskBtn = document.getElementById("addNewTaskBtn")
 addNewTaskBtn.addEventListener("click", taskAddition)
 
+
+
+///Global variables///
+const taskInput = document.getElementById("taskInput");
+const openTasks = document.getElementById("openTaskList");
+const pastTasks = document.getElementById("pastTaskList");
+
+
+////Functions///
+// New tasks
 function taskAddition(e) {
     e.preventDefault();
-    
-    const taskInput = document.getElementById("taskInput");
-    const openTasks = document.getElementById("openTaskList");
-    const warningText = document.getElementById("warningText");
-
     if (taskInput.value.trim()) {
+        //Create a new list item
         let newListItem = document.createElement("div");
         newListItem.classList.add("list-item")
         newListItem.innerHTML = `<li>${taskInput.value.trim()}</li>
-        <button type="submit" class="btn button btn-success" id="completeTaskBtn"><i class="fa-solid fa-check"></i></button>
-        <button type="submit" class="btn button btn-danger" id="cancelTaskBtn"><i class="fa-solid fa-x"></i></button>`;
+        <button type="button" class="btn button btn-success" id="completeTaskBtn"><i class="fa-solid fa-check"></i></button>
+        <button type="button" class="btn button btn-danger" id="cancelTaskBtn"><i class="fa-solid fa-x"></i></button>`;
         openTasks.appendChild(newListItem);
         
+        //Reset the input box
         taskInput.value = "";
-        // warningText.innerText = "";
         taskInput.placeholder = "What do you need to do?";
         taskInput.classList.remove("warning");
         taskInput.focus();
+
+        // Register the new buttons as event listeners
+        const newCompleteBtn = newListItem.querySelector("#completeTaskBtn");
+        const newCancelBtn = newListItem.querySelector("#cancelTaskBtn");
+        newCompleteBtn.addEventListener("click", taskComplete);
+        newCancelBtn.addEventListener("click", taskCancel);
     } else {
-        // warningText.innerText = "Task is missing content";
+        //No task value, issue a warning
         taskInput.placeholder = "Task is missing content!";
         taskInput.classList.add("warning");
 
     }
 }
 
-//Complete tasks
-/// Event listener for complete btn
-
+// Complete tasks
 function taskComplete(e) {
-    const pastTasks = document.getElementById("pastTaskList")
     //Grab the content of the sibling?? li as a variable
     //append it to past tasks
     //delete it from open tasks
+    //focus on input
 }
 
-
-// Event listener to catch the Delete Task click (If this is oging to be per list item it needs to be in that function)
-// Function delete task
+// Delete task
+function taskCancel(e) {
+    if (confirm("Cancel this task?")) {
+    const taskToDelete = e.target.parentNode;
+    taskToDelete.parentNode.removeChild(taskToDelete);
+    taskInput.focus();
+    }
+}
